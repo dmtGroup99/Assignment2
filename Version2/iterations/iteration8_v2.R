b624d59b2c432bea360d3dd0d7f702e9001a6e4c
mdeblaauw <mdeblaauw@mobiquityinc.com>
2018-05-07 22:52:13 +0200

    Little change in xgb.DMatrix (delete position and numeric) to run iteration8_v2 script

--- Version2/iterations/iteration8_v2.R
+++ Version2/iterations/iteration8_v2.R
@@ -3,7 +3,7 @@ library(data.table)
 data <- data.table::fread(input = "../training_set_VU_DM_2014.csv", na.strings = c("NA", "NULL", "null"), data.table = FALSE)
 
 #preprocess data
-source("data_scripts_v2/iteration8_v2.R")
+source("Version2/data_scripts_v2/iteration8_v2.R")
 data_new <- iteration8_v2(data, 0.8)
 
 #train xgboost
@@ -12,8 +12,9 @@ library(xgboost)
 train <- data_new$train[order(data_new$train$srch_id),]
 counts <- data.frame(table(data_new$train$srch_id))
 
-dtrain <- xgb.DMatrix(as.matrix(train[,-c("srch_id", "target")]), label = train$target, group = counts$Freq) #without srch_id, target 
-dtest <- xgb.DMatrix(as.matrix(data_new$test[,-c("srch_id", "target")]))
+#hoort de position erin? heb ik er nu uitgehaald
+dtrain <- xgb.DMatrix(as.matrix(train[,-c(1, 22, 10)]), label = train$target, group = counts$Freq) #without srch_id, target 
+dtest <- xgb.DMatrix(as.matrix(data_new$test[,-c(1, 10, 22)]))
 
 #listwise model (lambdaMart)
 bst <- xgb.train(data = dtrain, max.depth = 5, eta = 1, nthread = 5, nround = 100, objective = "rank:pairwise", 
@@ -29,5 +30,5 @@ predictions <- predict(bst,dtest)
 id <- data_new$test$srch_id
 label <- data_new$test$target
 
-source("ndcg_function.R")
+source("Version2/ndcg_function.R")
 metric <- ndcg(label, id, predictions, 38)
